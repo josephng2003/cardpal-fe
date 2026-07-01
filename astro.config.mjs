3d6e7e4b0c6522094fc181f59134a334c69d717c
Phong Nguyen <andyhoaiphong2003@gmail.com>
2026-07-02 00:07:51 +0700

    feature: add react and sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import sentry from '@sentry/astro'
+
+import react from '@astrojs/react'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sentry({
+      project: 'cardpal',
+      org: 'decoteam',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+    react(),
+  ],
+})
