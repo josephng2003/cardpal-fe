971280a7a63ab9da5819a5c2cdbe9c02a088032c
Phong Nguyen <andyhoaiphong2003@gmail.com>
2026-07-07 22:46:38 +0700

    Add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import sentry from '@sentry/astro'
-
+import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [
     sentry({
       project: 'cardpal',
